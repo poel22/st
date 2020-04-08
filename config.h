578df5487c67094bce1b48e8976ba0fca5249c71
paul <paul@nextpart.io>
2020-04-08 08:49:54 +0200

    change default font

--- config.h
+++ config.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=26:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=20:antialias=true:autohint=true" };
+static char *font = "Inconsolata for Powerline:pixelsize=30:antialias=true:autohint=true";
+static char *font2[] = {"Inconsolata for Powerline:pixelsize=30:antialias=true:autohint=true" };
+// static char *font2[] = { "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
